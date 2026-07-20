f8031f186b51ca56de4fd167d9e6e01cc3df4d20
Dlopez2910 <dportilho54@gmail.com>
2026-07-20 12:16:09 -0400

    adicionado conversao de valores

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 const convertButton = document.querySelector('.convert-button');
 
-function convertvalues(){  
+function convertvalues() {
     const inputCurrencyValue = document.querySelector('.input-currency').value;
-   
+    const currencyValueReal = document.querySelector('.currency-value-real');
+    const currencyValueDolar = document.querySelector('.currency-value-dolar');
+
     const dolarToday = 5.2;
-   
 
 
-const convertedDolarValue = inputCurrencyValue / dolarToday;
 
-    
+    const convertedDolarValue = inputCurrencyValue / dolarToday;
+
+    currencyValueReal.innerHTML = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(inputCurrencyValue);
+    currencyValueDolar.innerHTML = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(convertedDolarValue);
+
     console.log(convertedDolarValue);
-    
+
 }
 
 convertButton.addEventListener("click", convertvalues); 
